d1a4c427d24b606f6d33a862617e0efa5035dc22
Andristlt <andristlt@gmail.com>
2023-10-31 22:29:41 +0400

    2

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-const widht = 10;
-const height = 4;
-const space = widht * height;
-console.log(space)
+// let a = 5;
+// let b = 5.6;
+// console.log(typeof b);
+// a = 'Hello';
+// console.log(typeof a);
+// let isAdmin = false;
+// console.log(typeof isAdmin);
+
+
+// let c;
+// console.log(typeof c);
+
+
+// let d = null;
+// console.log(typeof d);
+
+const chas = 80;
+let time = 5;
+let dey = 5;
+let of = 11;
+let zakaz = 40;
+console.log((time * of) > zakaz);
+console.log(chas * zakaz);
